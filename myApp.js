8f37ddfd6a8e5bb7edc38457a28ab50cb1324490
Nadthapon Sukeewadthana <38154439+teraflops-droid@users.noreply.github.com>
2022-07-11 05:36:06 +0000

    Add tutorial about using express

--- myApp.js
+++ myApp.js
@@ -1,11 +1,60 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
+let bodyParser = require('body-parser');
+
+//Implement a Root-Level Request Logger Middleware
+app.use((req,res,next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip );
+  next();
+})
+
+//Serve static assets
+app.use('/public', express.static(__dirname + "/public"));
+
+//serve html file
+app.get("/", (req,res) => {
+  res.sendFile(__dirname + "/views/index.html"); 
+})
+
+//serve json data and use env variable to check
+app.get("/json", (req, res) => {
+  if(process.env.MESSAGE_STYLE === 'uppercase') {
+  res.json({message: "Hello json".toUpperCase()});  
+  } 
+  else {res.json({message: "Hello json"});
+}
+})
+
+//Chain Middleware to Create a Time Server
+app.get('/now', (req,res,next) => {
+  req.time = new Date().toString();
+  next();
+  }, (req,res) => {
+    res.send({time: req.time});
+  });
+
+//Get Route Parameter Input from the Client
+app.get('/:word/echo', (req,res) => {
+  let {word} = req.params;
+  res.send({echo:word})
+})
+
+//Get Query Parameter Input from the Client
+app.get('/name', (req,res) => {
+  let {first, last} = req.query;
+  res.send({name: `${first} ${last}`
+           });
+})
 
 
+//Use body-parser to Parse POST Requests
+app.use(bodyParser.urlencoded({ extended: false}));
 
-
-
+//Get Data from POST Requests
+app.post('/name', bodyParser.json(),(req,res) => {
+  var stringName = req.body.first + " " + req.body.last;
+  res.json({ name: stringName });
+})
 
 
 
--- server.js
+++ server.js
@@ -8,9 +8,6 @@ var myApp = require('./myApp');
 var express = require('express');
 var app = express();
 
-app.get("/", function(req,res) {
-  res.send('Hello Express');
-})
 
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
